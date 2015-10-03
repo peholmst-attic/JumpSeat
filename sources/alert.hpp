7ef8f3a602eeddce4904fb43d67c830c9262320a
Petter Holmstroem <peholmst@abo.fi>
2015-10-03 09:11:54 +0300

    Added documentation to alert.hpp

--- sources/alert.hpp
+++ sources/alert.hpp
@@ -10,20 +10,27 @@
 
 namespace JumpSeat {
 
+    /**
+     * A structure containing information about an alert.
+     */
     struct Alert {
-        AlertType type;
-        boost::optional<std::string> municipality;
-        boost::optional<std::string> address;
-        boost::optional<std::string> details;
-        std::string message;
-        DateTime timestamp;
+        AlertType type; /**< the type of the alert (e.g. structure fire, MVA, automatic fire alarm, etc.) */
+        boost::optional<std::string> municipality; /**< the municipaliy in which the incident occured, if available */
+        boost::optional<std::string> address; /**< the address of the incident, if available */
+        boost::optional<std::string> details; /**< the details of the incident, if available */
+        std::string message; /**< the complete original alert message */
+        DateTime timestamp; /**< the date and time when the alert was received */
     };
 
+    /**
+     * An enumeration of the different fields in an alert message. It
+     * is used when parsing an arriving alert message into an Alert structure.
+     */
     enum class AlertField {
-        code,
-        municipality,
-        address,
-        details
+        code, /**< the alert code to be passed to the AlertTypeRepository when retrieving the AlertType */
+        municipality, /**< the municipality of the incident */
+        address, /**< the address of the incident */
+        details /**< any other details about the incident */
     };
 
     typedef boost::signals2::signal<void (const Alert&) > OnAlert;
